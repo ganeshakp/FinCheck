60cdb206c9063afa7d38a7321e954da27b24888e
Ganesha <ganeshakp.51@gmail.com>
2026-07-16 17:17:20 +0530

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react';
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if(!PUBLISHABLE_KEY){
+  throw new Error("Publishable key not found");
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider>
+      <App />
+    </ClerkProvider>    
+  </StrictMode>,
+)
